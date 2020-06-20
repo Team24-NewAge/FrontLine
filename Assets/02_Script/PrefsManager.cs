1b9ff67607d3c5e1be912ab37b274f6116b61974
arena5045 <64789660+arena5045@users.noreply.github.com>
2020-06-20 15:44:45 +0900

    씬 수정

---
+++ Assets/02_Script/PrefsManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class PrefsManager
+{
+    private const string InventoryKey = "Inventory";
+
+    public static void Save_Inventory(string inven)
+    {
+        PlayerPrefs.SetString(InventoryKey, inven);
+    }
+
+
+}
