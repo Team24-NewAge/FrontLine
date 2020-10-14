b0551462dd665bdfac1810c7a55abe5299804654
SwordMasterHwang <64788553+SwordMasterHwang@users.noreply.github.com>
2020-10-14 13:02:49 +0900

    Revert "애미"
    This reverts commit c88c83ab4db394a83cb593621fb43318af0302b8.

---
+++ Assets/02_Script/ADD/UnitSpawnBtn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawnBtn : MonoBehaviour
+{
+    public bool btnanycheck = false;
+    public bool btnanyclick = false;
+
+    public GameObject summonmanager;
+    
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        summonmanager.GetComponent<UnitSummon>().anypush = this.gameObject;//눌린 버튼의 오브젝트 할당
+        btnanycheck = true;//버튼 눌림 확인
+    }
+
+    void Update()
+    {
+        if (btnanyclick)
+        {
+            this.gameObject.SetActive(false);//버튼 비활성화
+        }
+    }
+}
