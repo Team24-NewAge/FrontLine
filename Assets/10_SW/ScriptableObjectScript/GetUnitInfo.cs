58564f373b69b6a07f5350537d8c39c228a3c088
Lee SeungWon <loboson395@naver.com>
2020-11-07 02:22:07 +0900

    이승원: 인벤토리 합성에 맞게 수정

--- Assets/10_SW/ScriptableObjectScript/GetUnitInfo.cs
+++ Assets/10_SW/ScriptableObjectScript/GetUnitInfo.cs
@@ -7,7 +7,7 @@ public class GetUnitInfo : MonoBehaviour
     public static GetUnitInfo Instance { get; private set; }    //싱글톤?
 
     public GameObject Target;   //inspector창에 InventoryMN이 들어간 오브젝트를 넣기
-    CheckInvenManager.UnitDataClassification[] otherUDC;  //유닛 데이터를 담은 구조체UnitDataClassification으로 선언
+    CheckingInvenManager.UnitDataIdentification[] otherUDID;  //유닛 데이터를 담은 구조체UnitDataClassification으로 선언
 
 
     private void Awake()
@@ -17,7 +17,7 @@ public class GetUnitInfo : MonoBehaviour
 
     private void Start()
     {
-        otherUDC = Target.GetComponent<CheckInvenManager>().out_UDC;  //InventoryMN의 데이터 받음
+        otherUDID = Target.GetComponent<CheckingInvenManager>().out_UDID;  //InventoryMN의 데이터 받음
     }
 
 
@@ -26,54 +26,54 @@ public class GetUnitInfo : MonoBehaviour
     //유닛 코드
     public int getUnitCode(int n)   
     {
-        return otherUDC[n].code;
+        return otherUDID[n].code;
     }
 
     //유닛 이름
     public string getUnitName(int n)
     {
-        return otherUDC[n].name;
+        return otherUDID[n].name;
     }
     //유닛 등급
     public int getUnitGrade(int n)
     {
-        return otherUDC[n].grade;
+        return otherUDID[n].grade;
     }
 
     //유닛 설명
     public string getUnitDescript(int n)
     {
-        return otherUDC[n].descript;
+        return otherUDID[n].descript;
     }
 
     //유닛 체력
     public int getUnitHp(int n)
     {
-        return otherUDC[n].hp;
+        return otherUDID[n].hp;
     }
 
     //유닛 공격력
     public int getUnitAtk(int n)
     {
-        return otherUDC[n].atk;
+        return otherUDID[n].atk;
     }
 
     //유닛 방어력
     public int getUnitDef(int n)
     {
-        return otherUDC[n].def;
+        return otherUDID[n].def;
     }
 
     //유닛 공격속도
     public int getUnitAtkSp(int n)
     {
-        return otherUDC[n].atkSp;
+        return otherUDID[n].atkSp;
     }
 
     //유닛 가격
     public int getUnitpPrice(int n)
     {
-        return otherUDC[n].price;
+        return otherUDID[n].price;
     }
 
 }
