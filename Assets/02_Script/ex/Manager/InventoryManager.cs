5dd639cbd19fefe28d836e9a3896ed9a6a45de27
Lee SeungWon <loboson395@naver.com>
2020-11-11 16:14:06 +0900

    이승원: 02의 InventoryManager.cs 내용 추가 및 내용에맞게 InGame 수정

--- Assets/02_Script/ex/Manager/InventoryManager.cs
+++ Assets/02_Script/ex/Manager/InventoryManager.cs
@@ -7,6 +7,9 @@ public class InventoryManager : MonoBehaviour
 
     //여기에 아무 변수 추가
 
+    //(승원)변수 정의
+    public GameObject invenUI, fusionUI, reinforceUI;   //on,off할 패널
+
     public static InventoryManager Instance { get; private set; }
 
     public void Awake()
@@ -16,28 +19,35 @@ public class InventoryManager : MonoBehaviour
 
     public void Open_Inventory()//인벤토리 열어주는 매서드
     {
-
+        invenUI.transform.SetAsLastSibling();
+        invenUI.SetActive(true);
     }
 
     public void Close_Inventory()//인벤토리 닫는 매서드
     {
-
+        invenUI.SetActive(false);
     }
     public void Open_Fusion()//합성창 여는 메서드
     {
-
+        Open_Inventory();
+        fusionUI.SetActive(true);
+        reinforceUI.SetActive(false);
     }
     public void Close_Fusion()//합성창 닫는 메서드
     {
-
+        Close_Inventory();
+        fusionUI.SetActive(false);
     }
     public void Open_Reinforce()//강화창 여는 메서드
     {
-
+        Open_Inventory();
+        fusionUI.SetActive(false);
+        reinforceUI.SetActive(true);
     }
     public void Close_Reinforce()//강화창 닫는 매서드
     {
-
+        Close_Inventory();
+        reinforceUI.SetActive(false);
     }
 
     // Start is called before the first frame update
