39f813d26ee289c3a39657c5723cc414de55e63f
Hwang geonwoo <ds2jln@naver.com>
2020-11-12 04:08:16 +0900

    갱신 오류 해결
    처음 유닛 오브젝트에 유닛이 배치되어 있는 경우 갱신을 눌러도 해당 유닛은 표시되지 않게끔 설정함

--- Assets/02_Script/ADD/BtnAct.cs
+++ Assets/02_Script/ADD/BtnAct.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class BtnAct : MonoBehaviour
 {
     public GameObject[] btns = new GameObject[100];
+    public static bool unitbatch = true;// 취소버튼과 연동되어 배치취소를 눌러야만 처음 미리 배치된 유닛들도 배치가 가능
+    private GameObject unit;
+    private int unitnum;
+
+    public void Start()
+    {
+        unit = GameObject.Find("Unit");
+        unitnum = unit.transform.childCount - 1;
+    }
+
     public void OnClick()
     {
-        for (int i = 0; i < UnitSummon.tot_btn ; i++)
+        if (unitbatch)
+        {
+            for (int i = 0; i < unitnum ; i++)
             {
-                btns[i].SetActive(true);//버튼 활성화
-            }
+                btns[i].GetComponent<UnitSpawnBtn>().btnanycheck = true;
+                btns[i].GetComponent<UnitSpawnBtn>().btnanyclick = true;
+            }//취소버튼이 아직 눌리지 않았다면 유닛에 있는 자식 오브젝트(영웅제외) 관련 버튼 전부 비활성화
+        }
+        
+        for (int i = 0; i < UnitSummon.tot_btn ; i++)
+        {
+            btns[i].SetActive(true);//버튼 활성화'
+        }
             
     }
 }
--- Assets/02_Script/ADD/UnitCancelBtn.cs
+++ Assets/02_Script/ADD/UnitCancelBtn.cs
@@ -76,7 +76,7 @@ public class UnitCancelBtn : MonoBehaviour
         }
         
         CancleCheack = true;
-        
+        BtnAct.unitbatch = false;
     }
     
     
